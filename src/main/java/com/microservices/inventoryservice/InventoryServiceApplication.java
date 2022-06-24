c86eb9b2a3b0c7e8fca3dd5c620b114ac18b537e
Gabriel Oest <gabriel.oest@etermax.com.uy>
2022-06-24 00:21:44 -0300

    Add configuration to define instance random port number

--- src/main/java/com/microservices/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/microservices/inventoryservice/InventoryServiceApplication.java
@@ -1,14 +1,8 @@
 package com.microservices.inventoryservice;
 
-import com.microservices.inventoryservice.model.Inventory;
-import com.microservices.inventoryservice.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import java.beans.BeanProperty;
 
 @SpringBootApplication
 @EnableEurekaClient
